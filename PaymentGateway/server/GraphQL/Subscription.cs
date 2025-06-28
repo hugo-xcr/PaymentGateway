89a3dc9651e2430de71527a504b109b758095f82
hugo-xcr <hugo5960@yandex.ru>
2025-06-28 19:47:58 +0300

    2

--- PaymentGateway/server/GraphQL/Subscription.cs
+++ PaymentGateway/server/GraphQL/Subscription.cs
@@ -1,1 +1,20 @@
-﻿ 
+﻿using HotChocolate;
+using HotChocolate.Types;
+using PaymentGateway.Server.Models;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Server.GraphQL
+{
+    public class Subscription
+    {
+        [Subscribe]
+        [Topic("{id}")]
+        public PaymentResult PaymentStatusChanged(
+            [ID] string id,
+            [EventMessage] PaymentResult payment)
+        {
+            Console.WriteLine($"Отправка обновления для платежа {id}: {payment.Status}");
+            return payment;
+        }
+    }
+}
